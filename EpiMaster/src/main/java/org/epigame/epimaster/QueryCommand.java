7a5c8b6c7c2415a99892d6efa951ca90ebbcc95f
SushiCannibale <lpn.marc@gmail.com>
2024-12-19 22:08:10 +0100

    feat(epimaster): QueryTest

---
+++ EpiMaster/src/main/java/org/epigame/epimaster/QueryCommand.java
@@ -0,0 +1,18 @@
+package org.epigame.epimaster;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class QueryCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+
+        return true;
+    }
+}
